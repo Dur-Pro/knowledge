645a049cfd05a60b993e1bd08d08712044442c1c
Benoît Vézina <benoit@vezina.biz>
2023-12-03 11:33:00 -0500

    port 17

--- document_page_reference/__manifest__.py
+++ document_page_reference/__manifest__.py
@@ -5,11 +5,7 @@
     "name": "Document Page Reference",
     "summary": """
         Include references on document pages""",
-<<<<<<< HEAD
     "version": "17.0.1.0.0",
-=======
-    "version": "16.0.1.0.1",
->>>>>>> 77fead85e1b2b468438f3130e2f9630b2e7aebe6
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/knowledge",
